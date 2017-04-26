9ff47303c68e9c5cb0bc051e33cedf359b484e77
rorz <rory.mcmeekin@me.com>
2017-04-26 13:08:03 +0100

    refined clients entries and added scrolldown link and development note

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import { Link } from 'react-router';
 // import { prefixLink } from 'gatsby-helpers';
 import { Container } from 'react-responsive-grid';
@@ -10,16 +11,20 @@ import { StickyContainer, Sticky } from 'react-sticky';
 
 import { Link, Element } from 'react-scroll';
 
+import Reveal from 'react-reveal';
+
 import Starfield from '../components/Starfield';
 import TypistCycle from '../components/TypistCycle';
 
 import './styles.scss';
 import '../node_modules/react-typist/dist/Typist.css';
+import 'animate.css/animate.css';
 
 import mouseIcon from './mouse-icon.svg';
 import testShipIcon from './test-ship.png';
 import testServiceIcon from './test-service-icon.png';
 
+import mockupSM from './mockup-sm-01.jpg';
 // const Scroll = require('react-scroll');
 //
 // const Link = Scroll.Link;
@@ -59,9 +64,14 @@ function StarfieldOverlay() {
         segmentDelay={0.8}
         className="starfield-byline"
       />
-      <button>
-        Scroll Down
-      </button>
+      <Link
+        className="scroll-down-link blink_me"
+        to="intro_pane"
+        smooth
+        duration={1800}
+      >
+        &darr;&nbsp;SCROLL DOWN&nbsp;&darr;
+      </Link>
     </div>
   );
 }
@@ -103,6 +113,7 @@ function StickyNav(props) {
         borderBottomWidth: '1px',
         borderBottomStyle: 'solid',
         boxShadow: '0 0 8px 0 rgba(0,0,0,.2), 0 0 4px 0 rgba(0,0,0,.19)',
+        zIndex: '9002',
       }}
     >
       <WidthMaster>
@@ -264,64 +275,85 @@ function ServicesPane() {
   );
 }
 
+function SlideIcon(props) {
+  const invert = props.invert;
+  const animation = !invert ? [
+    'fadeInLeft',
+    'fadeInRight',
+  ] : [
+    'fadeInRight',
+    'fadeInLeft',
+  ];
+
+  console.log(`animation ${animation[0]}`);
+
+  let layout = [
+    (<Reveal key={animation[0]} style={{zIndex: 0}} className="one-two-icon" effect={`animated ${animation[0]}`}>
+      <img src={props.icon} alt="clients_image" />
+    </Reveal>),
+    (<Reveal key={animation[1]} className="one-two-para" effect={`animated ${animation[1]}`}>
+      <div
+        style={{
+          textAlign: (invert ? 'right' : 'left'),
+          padding: (invert ? '1rem 3rem 2rem 0' : '1rem 0 2rem 3rem'),
+        }}
+      >
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+      </div>
+    </Reveal>),
+  ];
+
+
+  if (invert) {
+    layout = layout.reverse();
+  }
+
+  return (
+    <div className="one-two">
+      {layout}
+    </div>
+  );
+}
+SlideIcon.propTypes = {
+  invert: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+
 function ClientsPane() {
   return (
     <div className="section">
       <div className="services-pane">
         <h2>Some things we&apos;ve built:</h2>
-        <div className="one-two">
-          <div className="one-two-icon">
-            <p>hallo</p>
-          </div>
-          <div className="one-two-para">
-            <p>hallo</p>
-          </div>
-        </div>
-        <div className="blockContainer">
-          <p>
-            At Apollo27, we take a hands-on and holistic approach to making your idea a reality.
-            Having primarily worked with start-ups,
-            we know how fast-paced and dynamic work needs to be.
-            We&apos;re also well-versed in positioning the content we create
-            towards the audience you need; whether it be investors, clients, or users.
-          </p>
-          <div className="columns">
-            <div className="solutionColumn">
-              <img alt="test" src={testShipIcon} width={200} />
-              <h3>
-                Planning Phase
-              </h3>
-              <p>
-                We take meticulous care over the planning of your idea
-                and want to make sure it succeeds!
-                We won&apos;t stop at anything to see it planned.
-              </p>
-            </div>
-            <div className="solutionColumn">
-              <img alt="test" src={testShipIcon} width={200} />
-              <h3>
-                Building Phase
-              </h3>
-              <p>
-                You take meticulous care over the building of your idea
-                and want to make sure it succeeds!
-                We won&apos;t stop at anything to see it built.
-              </p>
-            </div>
-            <div className="solutionColumn">
-              <img alt="test" src={testShipIcon} width={200} />
-              <h3>
-                Launch Phase
-              </h3>
-              <p>
-                I take meticulous care over the launching of your idea
-                and want to make sure it succeeds!
-                We won&apos;t stop at anything to see it!
-              </p>
-            </div>
-          </div>
-        </div>
+        <SlideIcon
+          title="SkyMining®"
+          description="More hello-worldy-stuff"
+          icon={mockupSM}
+        />
+        <SlideIcon
+          title="EcoServices"
+          description="More hello-worldy-stuff"
+          icon={testShipIcon}
+          invert
+        />
+        <SlideIcon
+          title="HeadMarket"
+          description="More hello-worldy-stuff"
+          icon={testShipIcon}
+        />
       </div>
+    </div>
+  );
+}
+
+function UnderDevelopmentSign() {
+  return (
+    <div className="under-development-box">
+      <h3>This site is currently <strong>under development</strong></h3>
+      <p><Link>Please let us know if you find something out of place.</Link></p>
     </div>
   );
 }
@@ -402,6 +434,7 @@ export default class Index extends React.Component {
         <Element name="clients_pane" className="element">
           <ClientsPane />
         </Element>
+        <UnderDevelopmentSign />
       </StickyContainer>
     );
   }

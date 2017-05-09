df82b1728cd2025a7189e8c734399f3b10cf7cc5
rorz <rory.mcmeekin@me.com>
2017-05-09 12:11:25 +0100

    changed site base description and added GA

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+import ReactGA from 'react-ga';
+import { config } from 'config';
+
+ReactGA.initialize(config.googleAnalyticsId);
+
+exports.onRouteUpdate = (state) => {
+  ReactGA.pageview(state.pathname);
+  // assessPath(state.pathname)
+};
